d483b2166e0d0bbf3966c480e35ab27768c6c173
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2022-03-04 01:14:34 +0300

    Implemented smooth scrolling.

--- asssets/app.js
+++ asssets/app.js
@@ -4,17 +4,38 @@ $(function() {
         top = $("#header__top"),
         headerH = $("#header").innerHeight(),
         topH = $("#header__top").innerHeight(),
-        scrollOffset = 0;
+        scrollOffset = $(window).scrollTop();;
+
+    //  Fixed Header (top)
+    checkScroll(scrollOffset);
 
     $(window).on("scroll", function(){
-
         scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+    });
+
+    function checkScroll(scrollOffset){
 
         if( scrollOffset >= headerH - topH){
             top.addClass("fixed");
         } else {
             top.removeClass("fixed");
         }
+    }
+
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var blockId = $(this).data("scroll"),
+            blockOffset = $(blockId).offset().top;
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
     });
 
 });
+
+// will add active class for a link; maybe
